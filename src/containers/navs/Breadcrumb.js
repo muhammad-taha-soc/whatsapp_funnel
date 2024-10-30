4189ced36cde5aecf7aa37300f54d396ed84e905
farrukhazeem123 <farrukh.azeem@soc-solution.com>
2024-10-30 14:05:47 +0500

    conflict resolve

--- src/containers/navs/Breadcrumb.js
+++ src/containers/navs/Breadcrumb.js
@@ -15,46 +15,19 @@ import IntlMessages from 'helpers/IntlMessages';
 // };
 
 const BreadcrumbContainer = ({ heading }) => {
+
   return (
     <>
       {heading && (
-
-        <div className="coupon-heading pb-0">
-          {heading}
-          {/* <IntlMessages style={{color:"red"}} id={heading} /> */}
-        </div>
-
+        <h1>
+          <IntlMessages id={heading} />
+        </h1>
       )}
       {/* <BreadcrumbItems match={match} /> */}
     </>
   );
-};
+  }
 
-// const BreadcrumbItems = ({ match }) => {
-//   const path = match.path.substr(1);
-//   let paths = path.split('/');
-//   if (paths[paths.length - 1].indexOf(':') > -1) {
-//     paths = paths.filter((x) => x.indexOf(':') === -1);
-//   }
-//   return (
-//     <>
-//       <Breadcrumb className="pt-0 breadcrumb-container d-none d-sm-block d-lg-inline-block">
-//         {/* {paths.map((sub, index) => {
-//           return (
-//             <BreadcrumbItem key={index} active={paths.length === index + 1}>
-//               {paths.length !== index + 1 ? (
-//                 <NavLink to={`/${getUrl(path, sub, index)}`}>
-//                   {getMenuTitle(sub)}
-//                 </NavLink>
-//               ) : (
-//                 getMenuTitle(sub)
-//               )}
-//             </BreadcrumbItem>
-//           );
-//         })} */}
-//       </Breadcrumb>
-//     </>
-//   );
-// };
+
 
 export default BreadcrumbContainer;
